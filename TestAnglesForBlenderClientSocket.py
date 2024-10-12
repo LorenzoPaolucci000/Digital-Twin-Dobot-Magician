d404cbac0e87f45c04bec242ccc5a67860f90064
LorenzoPaolucci000 <lorenzo.paolucci@studenti.unicam.it>
2024-10-12 17:04:04 +0200

    Delete useless files

--- TestAnglesForBlenderClientSocket.py
+++ TestAnglesForBlenderClientSocket.py
@@ -1,7 +1,5 @@
 import socket
 import time
-import math
-from math import radians
 
 # Connessione al server del Dobot
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -43,55 +41,3 @@ except KeyboardInterrupt:
 finally:
     client_socket.close()
     print("Client shutdown")
-
-# import bpy
-# import socket
-# import math
-# from math import radians
-#
-# # Connessione al server del Dobot
-# client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# client_socket.connect(('localhost', 65432))
-#
-# # Buffer per ricevere i dati dal server
-# buffer_size = 256
-#
-# # Nomi degli oggetti in Blender
-# joint_objects = {
-#     'Joint1': 'PivotArm',  # esempio di nome per Joint1, asse Z
-#     'Joint2': 'RearArm',  # esempio di nome per Joint2, asse Y
-#     'Joint3': 'ForeArm',  # esempio di nome per Joint3, asse Y
-# }
-#
-#
-# def update_joint_angles():
-#     try:
-#         data = client_socket.recv(buffer_size)  # Ricevi i dati dal server (buffer di 256 byte)
-#         if data:
-#             joint_angles = data.decode('utf-8').strip().split(',')
-#             angles = [radians(float(angle)) for angle in joint_angles]
-#
-#             # Applica gli angoli di rotazione ai giunti corretti
-#             if joint_objects['Joint1'] in bpy.data.objects:
-#                 bpy.data.objects[joint_objects['Joint1']].rotation_euler[2] = math.radians(angles[0])  # Asse Z per Joint1
-#             if joint_objects['Joint2'] in bpy.data.objects:
-#                 bpy.data.objects[joint_objects['Joint2']].rotation_euler[1] = math.radians(-(angles[1] - 26.925539016723633)) # Asse Y per Joint2
-#             if joint_objects['Joint3'] in bpy.data.objects:
-#                 bpy.data.objects[joint_objects['Joint3']].rotation_euler[1] = math.radians(-(angles[2] - 28.57915496826172))  # Asse Y per Joint3
-#
-#     except BlockingIOError:
-#         # Non ci sono dati disponibili, passiamo al prossimo ciclo
-#         pass
-#
-#     except Exception as e:
-#         print(f"Errore durante la ricezione dei dati: {e}")
-#
-#     # Richiama questa funzione dopo 0.1 secondi
-#     bpy.app.timers.register(update_joint_angles, first_interval=0.1)
-#
-#
-# # Imposta il socket in modalità non bloccante
-# client_socket.setblocking(False)
-#
-# # Avvia il timer per aggiornare gli angoli dei giunti
-# bpy.app.timers.register(update_joint_angles)
